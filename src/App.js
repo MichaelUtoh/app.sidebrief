51bba6f3e85cb1e2e5e14857ab91e23ee75a0874
michael <michael.utoh@torilo.co.uk>
2021-12-27 23:31:38 +0100

    Folder setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import HeaderComponent from './components/Navigation/Header';
+import NavbarComponent from './components/Navigation/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 min-h-screen">
+      <HeaderComponent title="Home" />
+      <NavbarComponent />
     </div>
   );
 }
---
+++ src/components/Navigation/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeaderComponent = ({title}) => {
+    return (
+        <head className="">
+            <title>{title} | Sidebrief</title>
+        </head>
+    )
+}
+
+export default HeaderComponent
